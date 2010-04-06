a8a9403d7318a1aa04e8a4859f9a603611a56c0e
Sunteya <Sunteya@gmail.com>
2010-04-06 11:17:55 +0800

    add gem depends

---
+++ depends.rb
@@ -0,0 +1,30 @@
+require "rubygems"
+gem "gem_loader", ">= 0.2.0"
+require "gem_loader"
+
+
+GemLoader.setup do
+
+  scope :runtime do
+    gem "capistrano", ">= 2.5.14"
+    gem "capistrano_colors", ">= 0.5.2", :require => nil
+    gem "capistrano-ext", ">= 1.2.1", :require => nil
+    gem "capistrano-helpers", ">= 0.3.2", :require => nil
+    # gem "cap-recipes", ">= 0.3.36", :require => nil
+  end
+
+  scope :optional do
+  end
+  
+  scope :test do
+    # gem "rspec", ">= 1.3.0", :require => nil
+  end
+
+  scope :development => [:optional, :test]
+
+  scope :rakefile do
+    gem "rake", ">= 0.8.7"
+    gem "jeweler", ">= 1.4.0"
+    # gem "rspec", :require => "spec/rake/spectask"
+  end
+end
---
+++ lib/capsum.rb
@@ -0,0 +1,2 @@
+require File.expand_path("../../depends.rb", __FILE__)
+GemLoader.require(:runtime)
