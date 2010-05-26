ecd942ff57c47aaf150eb47d747fe1ccc14fe32b
Sunteya <Sunteya@gmail.com>
2010-05-26 10:20:06 +0800

    passenger now ignore start and stop task

--- lib/capsum/passenger.rb
+++ lib/capsum/passenger.rb
@@ -6,5 +6,15 @@ Capistrano::Configuration.instance(true).load do
     task :restart, :roles => :app do
       run "touch #{current_path}/tmp/restart.txt"
     end
+    
+    desc 'Start passenger'
+    task :start, :roles => :app do
+      # do nothing
+    end
+    
+    desc 'Stop passenger'
+    task :stop, :roles => :app do
+      # do nothing
+    end
   end
 end
