0b7a9441cc7a8512e4f77c653ea878035a98f287
Sunteya <Sunteya@gmail.com>
2010-05-26 11:18:19 +0800

    whenever run at app role

--- lib/capsum/whenever.rb
+++ lib/capsum/whenever.rb
@@ -3,14 +3,14 @@ require File.expand_path("../../capsum.rb", __FILE__)
 Capistrano::Configuration.instance(true).load do
   namespace :whenever do
     desc 'Update the crontab'
-    task :update, :roles => :db do
+    task :update, :roles => :app do
       command = "if [ -e #{current_path}/config/schedule.rb ]; then cd #{current_path}; RAILS_ENV=#{rails_env} whenever --update-crontab #{application} --set primary=%s; fi"
       run (command % true), :only => { :primary => true }
       run (command % false), :except => { :primary => true }
     end
     
     desc 'Cleanup the crontab'
-    task :cleanup, :roles => :db do
+    task :cleanup, :roles => :app do
       run "if [ -e #{current_path}/config/schedule.rb ]; then cd #{current_path}; RAILS_ENV=#{rails_env} whenever --update-crontab #{application} --load-file /dev/null; fi"
     end
   end
