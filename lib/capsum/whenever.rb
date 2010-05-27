d3fba3dff863982d1fef04501d42155943973ea2
Sunteya <Sunteya@gmail.com>
2010-05-27 09:47:51 +0800

    fix whenever no primary app server deploy error

--- lib/capsum/whenever.rb
+++ lib/capsum/whenever.rb
@@ -3,10 +3,11 @@ require File.expand_path("../../capsum.rb", __FILE__)
 Capistrano::Configuration.instance(true).load do
   namespace :whenever do
     desc 'Update the crontab'
-    task :update, :roles => :app do
+    task :update do
       command = "if [ -e #{current_path}/config/schedule.rb ]; then cd #{current_path}; RAILS_ENV=#{rails_env} whenever --update-crontab #{application} --set primary=%s; fi"
-      run (command % true), :only => { :primary => true }
-      run (command % false), :except => { :primary => true }
+      find_servers(:roles => :app).each do |server|
+        run (command % (server.options[:primary] == true)), :hosts => server.host
+      end
     end
     
     desc 'Cleanup the crontab'
