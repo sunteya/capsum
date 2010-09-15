17795b21db44e0e7f22c6e72866f789b76da5066
Sunteya <Sunteya@gmail.com>
2010-09-15 10:17:30 +0800

    update whenever recipe support setting variables on the fly

--- lib/capsum/whenever.rb
+++ lib/capsum/whenever.rb
@@ -4,9 +4,17 @@ Capistrano::Configuration.instance(true).load do
   namespace :whenever do
     desc 'Update the crontab'
     task :update do
-      command = "if [ -e #{current_path}/config/schedule.rb ]; then cd #{current_path}; RAILS_ENV=#{rails_env} whenever --update-crontab #{application} --set primary=%s; fi"
       find_servers(:roles => :app).each do |server|
-        run (command % (server.options[:primary] == true)), :hosts => server.host
+        whenever_settings = server.options[:whenever]
+        if whenever_settings
+          variables = whenever_settings.to_s
+          if whenever_settings.is_a?(Hash)
+            variables = whenever_settings.map{ |key, value| "#{key}=#{value}" }.join("&")
+          end
+
+          command = "if [ -e #{current_path}/config/schedule.rb ]; then cd #{current_path}; RAILS_ENV=#{rails_env} whenever --update-crontab #{application} --set %s; fi"
+          run (command % variables), :hosts => server.host
+        end
       end
     end
     
