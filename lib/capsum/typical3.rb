8438fa6ce4f82c0aad79c76685b331d1d63ed7e2
Sunteya <Sunteya@gmail.com>
2010-11-02 15:01:50 +0800

    fix typical 3 init error

--- lib/capsum/typical3.rb
+++ lib/capsum/typical3.rb
@@ -3,6 +3,7 @@ require File.expand_path("../../capsum.rb", __FILE__)
 require File.expand_path("../typical.rb", __FILE__)
 require "bundler/capistrano"
 
-set :bundle_flags, "--quiet"
-set :bundle_dir, ""
-
+Capistrano::Configuration.instance(true).load do
+  set :bundle_flags, "--quiet"
+  set :bundle_dir, ""
+end
