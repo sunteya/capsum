d69fc10578ba0d10ab9275cda1374a21f4caaaa3
Sunteya <Sunteya@gmail.com>
2011-01-13 09:38:24 +0800

    fix bundler don't fetch correct "current_release" value

--- lib/capsum/bundler.rb
+++ lib/capsum/bundler.rb
@@ -3,9 +3,9 @@ require "bundler/deployment"
 
 Capistrano::Configuration.instance(true).load do
   
-  
   set :bundle_flags, "--quiet #{fetch(:bundle, '')}"
   set :bundle_dir, ""
+  set(:current_release) { fetch(:release_path) }
   
   Bundler::Deployment.define_task(self, :task, :except => { :no_release => true })
 
