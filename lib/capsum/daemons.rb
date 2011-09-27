8ba318c27ae1525308fecc4780764016903f8bf6
Sunteya <Sunteya@gmail.com>
2011-09-27 16:08:07 +0800

    daemons support custom command

--- lib/capsum/daemons.rb
+++ lib/capsum/daemons.rb
@@ -5,39 +5,109 @@ Capistrano::Configuration.instance(true).load do
   namespace :daemons do
     desc "Start daemons process" 
     task :start, :roles => :app do
-      daemons = fetch(:daemons, [])
       find_servers(:roles => :app).each do |server|
-        daemons_settings = server.options[:daemons]
-        
-        daemons.each do |daemon|
-          runnable = daemons_settings
-          runnable = daemons_settings.match(daemon) if daemons_settings.respond_to?(:match)
-          
-          run("cd #{current_path}; if [ -e #{daemon} ]; then RAILS_ENV=#{rails_env} #{daemon} start; fi", :hosts => server.host) if runnable
+        matcher = server.options[:daemons]
+        daemon_list.each do |daemon|
+          run "cd #{current_path}; #{daemon[:start]}", :hosts => server.host if match(matcher, daemon)
         end
-        
       end
     end
-
+    
     desc "Stop daemons process" 
     task :stop, :roles => :app do
-      daemons = fetch(:daemons, [])
-      daemons.each do |daemon|
-        run "cd #{current_path}; if [ -e #{daemon} ]; then RAILS_ENV=#{rails_env} #{daemon} stop; fi" 
+      daemon_list.each do |daemon|
+        run "cd #{current_path}; #{daemon[:stop]}" 
       end
     end
 
     desc "Restart daemons process" 
     task :restart, :roles => :app do
       daemons.stop
-      sleep(4)
+      sleep(3)
       daemons.start
     end
-  end
+    
+    desc "install daemons to cron"
+    task :update_cron, :roles => :app do
+      find_servers(:roles => :app).each do |server|
+        
+        identifier = "#{deploy_to}#daemons"
+        schedule_file_path = "%{schedule_dir}/schedule.daemons.rb"
+        
+        whenever_clear_command  = "type whenever > /dev/null 2>&1 && whenever --clear-crontab #{identifier} --load-file /dev/null"
+        whenever_update_command = "whenever --update-crontab #{identifier} --load-file #{schedule_file_path}"
+        
+        command = "if [ -e #{schedule_file_path} ]; then #{whenever_update_command}; else #{whenever_clear_command}; fi"
+        
+        # on_rollback do
+        #   schedule_dir = fetch(:previous_release) || release_path
+        #   run (command % { :schedule_dir => schedule_dir }), :hosts => server.host
+        # end
+        
+        matcher = server.options[:daemons]
+        daemon_commands = daemon_list.map do |daemon|
+          "cd #{current_path}; #{daemon[:start]}" if match(matcher, daemon)
+        end.compact
+        
+        if !daemon_commands.empty?
+          schedule_content=<<-EOF
+every(:reboot) do
+#{daemon_commands.map{ |cmd| "  command #{cmd.inspect}" }.join("\n")}
+end
+EOF
+          top.put schedule_content, (schedule_file_path % { :schedule_dir => release_path }), :hosts => server.host
+        end
+        
+        run (command % { :schedule_dir => release_path }), :hosts => server.host
+      end
+    end
+    
+    after "deploy:symlink", "daemons:update_cron" 
+    after "deploy:rollback", "daemons:update_cron"
 
-  after "deploy:start", "daemons:start" 
-  after "deploy:stop", "daemons:stop" 
-  after "deploy:restart", "daemons:restart"
+    after "deploy:start", "daemons:start" 
+    after "deploy:stop", "daemons:stop" 
+    after "deploy:restart", "daemons:restart"
+    
+    def match(matcher, daemon)
+      return matcher.call(daemon[:name]) if matcher.respond_to?(:call)
+      return matcher.match(daemon[:name]) if matcher.respond_to?(:match)
+      
+      !!matcher # to boolean
+    end
+    
+    def daemon_list
+      settings = fetch(:daemons, [])
+      
+      case settings
+      when Array
+        settings.map { |setting| parse_daemon(setting) }
+      when Hash
+        settings.map { |pair| parse_daemon(pair[1], :name => pair[0] )}
+      else
+        [ parse_daemon(settings) ]
+      end
+    end
+    
+    def parse_daemon(source, options = {})
+      daemon = {}
+      
+      case source
+      when Array
+        daemon[:start] = source.first
+        daemon[:stop] = source.last
+      when Hash
+        daemon = options.merge(source)
+      else
+        command = source.to_s
+        daemon[:start] = "#{command} start RAILS_ENV=#{rails_env}"
+        daemon[:stop] = "#{command} stop RAILS_ENV=#{rails_env}"
+      end
+      
+      daemon[:name] = daemon[:start] if daemon[:name].nil?
+      daemon
+    end
+  end
 
 end
 
