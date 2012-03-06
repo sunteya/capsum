148af2d0a01f2250ec0583c4997e205cd878f15e
Sunteya <Sunteya@gmail.com>
2012-03-06 09:57:56 +0800

    git recipe's repository now default use project remote url.

--- lib/capsum/git.rb
+++ lib/capsum/git.rb
@@ -5,7 +5,13 @@ Capistrano::Configuration.instance(true).load do
   set :scm, "git"
   
   # use current branch
-  set(:branch) { `git describe --contains --all HEAD`.chomp }
+  set(:branch) { `git describe --contains --all HEAD | tr -d '\n'` }
+
+  # use current remote
+  set(:repository) do
+    remote = `git config --get branch.#{self.branch}.remote | tr -d '\n'`
+    `git config --get remote.#{remote}.url | tr -d '\n'`
+  end
   
   set :scm_verbose, true
   
