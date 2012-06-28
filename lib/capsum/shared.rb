d55e4d28b1a7cbd6f1b2301170477c60080a80e0
Sunteya <Sunteya@gmail.com>
2012-06-28 09:46:54 +0800

    capistrano-helpers 0.7+ already fix performed late bug

--- lib/capsum/shared.rb
+++ lib/capsum/shared.rb
@@ -3,6 +3,4 @@ require "capistrano-helpers/shared"
 
 Capistrano::Configuration.instance(true).load do
   
-  unafter "deploy:update_code", "deploy:symlink_shared"
-  before 'deploy:finalize_update', 'deploy:symlink_shared'
 end
