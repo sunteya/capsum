fff35afb80ae0dac899c422d582acf77d5be9640
sunteya <sunteya@gmail.com>
2013-04-28 14:41:51 +0800

    use official "shared_children" option replace "capistrano-helpers/shared" recipe

--- lib/capsum/shared.rb
+++ lib/capsum/shared.rb
@@ -1,6 +1,8 @@
 require File.expand_path("../../capsum.rb", __FILE__)
-require "capistrano-helpers/shared"
 
 Capistrano::Configuration.instance(true).load do
-  
+
+  on :load do
+    set :shared_children, shared_children + shared if exists?(:shared)
+  end
 end
