74c574896b96c2c27781677e8041a3405f223355
sunteya <sunteya@gmail.com>
2013-07-14 20:57:23 +0800

    use whenever's gem provide recipe

--- lib/capsum/whenever.rb
+++ lib/capsum/whenever.rb
@@ -1,38 +1,13 @@
 require File.expand_path("../../capsum.rb", __FILE__)
 
+begin
+  require "whenever/capistrano"
+rescue LoadError => e
+  # skip
+end
+
 Capistrano::Configuration.instance(true).load do
-  namespace :whenever do
-    desc 'Update the crontab'
-    task :update do
-      identifier = fetch(:whenever_identifier, deploy_to)
-      find_servers(:roles => :app).each do |server|
-        whenever_settings = server.options[:whenever]
-        if whenever_settings
-          variables = whenever_settings.to_s
-          if whenever_settings.is_a?(Hash)
-            variables = whenever_settings.map{ |key, value| "#{key}=#{value}" }.join("&")
-          end
-
-          command = "if [ -e #{current_path}/config/schedule.rb ]; then cd #{current_path}; RAILS_ENV=#{rails_env} #{whenever_bin} --update-crontab #{identifier} --set \"environment=#{rails_env}&%s\"; fi"
-          run (command % variables), :hosts => server.host
-        end
-      end
-    end
-    
-    desc 'Cleanup the crontab'
-    task :cleanup, :roles => :app do
-      identifier = fetch(:whenever_identifier, deploy_to)
-      run "if [ -e #{current_path}/config/schedule.rb ]; then cd #{current_path}; RAILS_ENV=#{rails_env} #{whenever_bin} --update-crontab #{identifier} --load-file /dev/null; fi"
-    end
-
-    def whenever_bin
-      if fetch(:use_bundle, false)
-        "bundle exec whenever"
-      else
-        "whenever"
-      end
-    end
-  end
-  
-  after "deploy:create_symlink", "whenever:update"
+  set(:whenever_command) { fetch(:use_bundle, false) ? "#{fetch(:bundle_cmd, 'bundle')} exec whenever" : "whenever" }
+  set(:whenever_options) { { roles: fetch(:whenever_roles), only: { whenever: true } } }
+  set(:whenever_identifier) { deploy_to }
 end
