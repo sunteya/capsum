403c2198f3841668345ad20481f1f27272c0d92a
sunteya <sunteya@gmail.com>
2014-04-01 08:55:51 +0800

    bundler can set special environmental variables for http_proxys

--- lib/capsum/bundler.rb
+++ lib/capsum/bundler.rb
@@ -3,5 +3,17 @@ require "capistrano/bundler"
 namespace :load do
   task :defaults do
     set :bundle_flags, '--quiet'
+    set :bundle_env_variables, {}
+
+    bundle_env_variables[:http_proxy] = ENV["bundle_http_proxy"] if ENV["bundle_http_proxy"]
+    bundle_env_variables[:https_proxy] = ENV["bundle_https_proxy"] if ENV["bundle_https_proxy"]
+  end
+end
+
+module Capistrano
+  module DSL
+    def bundle_env_variables
+      fetch(:bundle_env_variables)
+    end
   end
 end
