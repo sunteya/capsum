27e67b4fd7537febf24ae45ece6484ca40585210
sunteya <sunteya@gmail.com>
2017-01-25 12:31:43 +0800

    cleanup local shared files before symlink task

--- lib/capsum/shared.rb
+++ lib/capsum/shared.rb
@@ -7,5 +7,17 @@ namespace :capsum do
     end
   end
 end
-
 Rake::Task["deploy:check"].enhance ["capsum:symlink_shared_deprecated"]
+
+namespace :capsum do
+  task "prepare_links" do
+    on release_roles :all do
+      [fetch(:linked_files), fetch(:linked_dirs)].compact.flatten.each do |path|
+        target = release_path.join(path)
+        execute :rm, target if test "[ -L #{target} ]"
+      end
+    end
+  end
+end
+
+before "deploy:symlink:linked_files", "capsum:prepare_links"
